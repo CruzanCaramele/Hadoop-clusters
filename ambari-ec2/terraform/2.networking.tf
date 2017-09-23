6fd6578b16cfa4f5fe542708cf408935c4ee2b9a
CruzanCaramele <letters2hamza@gmail.com>
2017-09-24 01:15:33 +0800

    add ambari server and security groups and rules

--- ambari-ec2/terraform/2.networking.tf
+++ ambari-ec2/terraform/2.networking.tf
@@ -2,7 +2,7 @@
 # VPC
 #--------------------------------------------------------------
 resource "aws_vpc" "ambari-vpc" {
-    cidr_block                  = "${var.cidr_block}"
+    cidr_block                  = "${var.vpc_cidr_block}"
     enable_dns_support          = "${var.dns_support}"
     enable_dns_hostnames        = "${var.dns_hostnames}"
 
@@ -137,7 +137,80 @@ resource "aws_route_table_association" "public-ambari-route-association" {
 #--------------------------------------------------------------
 # Security Groups
 #--------------------------------------------------------------
+resource "aws_security_group" "ambari-server-security" {
+    name                        = "ambari-server-security"
+    description                 = "ports allowed on the ambari server"
 
+    ingress {
+        to_port                 = 8080
+        protocol                = "tcp"
+        from_port               = 8080
+        cidr_blocks             = ["${var.public_cidr}"]
+    }
+
+    ingress {
+        to_port                 = 22
+        protocol                = "tcp"
+        from_port               = 22
+        cidr_blocks             = ["${var.public_cidr}"]   
+    }
+
+    ingress {
+        to_port                 = 65535
+        protocol                = "tcp"
+        from_port               = 0
+        cidr_blocks             = ["${var.vpc_cidr_block}"]
+    }
+
+    egress  {
+        to_port                 = 0
+        protocol                = "tcp"
+        from_port               = 0
+        cidr_blocks             = ["${var.public_cidr}"]
+    }
+
+    tags    {
+        Name                    = "ambari-server-security"
+    }
+
+    lifecycle   {
+        create_before_destroy   = true
+    }
+}
+
+resource "aws_security_group" "ambari-node-security" {
+    name                        = "ambari-node-security"
+    description                 = "ports allowed on the ambari server"
+
+    ingress {
+        to_port                 = 65535
+        protocol                = "tcp"
+        from_port               = 0
+        cidr_blocks             = ["${var.vpc_cidr_block}"]
+    }
+
+    ingress {
+        to_port                 = 50070
+        protocol                = "tcp"
+        from_port               = 50070
+        cidr_blocks             = ["${var.public_cidr}"]
+    }
+
+    egress  {
+        to_port                 = 0
+        protocol                = "tcp"
+        from_port               = 0
+        cidr_blocks             = ["${var.public_cidr}"]
+    }
+
+    tags    {
+        Name                    = "ambari-node-security"
+    }
+
+    lifecycle   {
+        create_before_destroy   = true
+    }
+}
 
 
 
