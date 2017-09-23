55bae802abad88167fd03f72a06f4334d3ce4867
CruzanCaramele <letters2hamza@gmail.com>
2017-09-23 13:50:14 +0800

    add public route table

--- ambari-ec2/terraform/2.networking.tf
+++ ambari-ec2/terraform/2.networking.tf
@@ -20,10 +20,34 @@ resource "aws_vpc" "ambari-vpc" {
 # Internet Gateway
 #--------------------------------------------------------------
 resource "aws_internet_gateway" "ambari-gateway" {
-    vpc_id      = "${aws_vpc.ambari-vpc.id}"
+    vpc_id                      = "${aws_vpc.ambari-vpc.id}"
 
     tags    {
-        Name    = "ambari-gateway"
+        Name                    = "ambari-gateway"
+    }
+
+    lifecycle   {
+        create_before_destroy   = true
+    }
+}
+
+#--------------------------------------------------------------
+# Route Tables
+#--------------------------------------------------------------
+resource "aws_route_table" "public-ambari-route-table" {
+    vpc_id                      = "${aws_vpc.ambari-vpc.id}"
+
+    route   {
+        cidr_block              = "${var.public_cidr}"
+        gateway_id              = "${aws_internet_gateway.ambari-gateway.id}"
+    }
+
+    tags    {
+        Name                    = "public-ambari-route-table"
+    }
+
+    lifecycle   {
+        create_before_destroy   = true
     }
 }
 
@@ -31,44 +55,57 @@ resource "aws_internet_gateway" "ambari-gateway" {
 # Network Access Control List
 #--------------------------------------------------------------
 resource "aws_network_acl" "ambari-acl" {
-    vpc_id          = "${aws_vpc.ambari-vpc.id}"
+    vpc_id                      = "${aws_vpc.ambari-vpc.id}"
 
     egress  {
-        action      = "allow"
-        rule_no     = "80"
-        to_port     = "0"
-        from_port   = "0"
-        protocol    = "-1"
-        cidr_block  = "${var.acl_cidr_block}"
+        action                  = "allow"
+        rule_no                 = "80"
+        to_port                 = "0"
+        from_port               = "0"
+        protocol                = "-1"
+        cidr_block              = "${var.acl_cidr_block}"
     }
 
     ingress  {
-        action      = "allow"
-        rule_no     = "80"
-        to_port     = "8080"
-        from_port   = "8080"
-        protocol    = "tcp"
-        cidr_block  = "${var.acl_cidr_block}"
+        action                  = "allow"
+        rule_no                 = "80"
+        to_port                 = "8080"
+        from_port               = "8080"
+        protocol                = "tcp"
+        cidr_block              = "${var.acl_cidr_block}"
     }
 
     ingress  {
-        action      = "allow"
-        rule_no     = "90"
-        to_port     = "22"
-        from_port   = "22"
-        protocol    = "tcp"
-        cidr_block  = "${var.acl_cidr_block}"
+        action                  = "allow"
+        rule_no                 = "90"
+        to_port                 = "22"
+        from_port               = "22"
+        protocol                = "tcp"
+        cidr_block              = "${var.acl_cidr_block}"
     }
 
     ingress  {
-        action      = "allow"
-        rule_no     = "100"
-        to_port     = "50070"
-        from_port   = "50070"
-        protocol    = "tcp"
-        cidr_block  = "${var.acl_cidr_block}"
+        action                  = "allow"
+        rule_no                 = "100"
+        to_port                 = "50070"
+        from_port               = "50070"
+        protocol                = "tcp"
+        cidr_block              = "${var.acl_cidr_block}"
+    }
+
+    tags    {
+        Name                    = "ambari-acl"
+    }
+
+    lifecycle   {
+        create_before_destroy   = true
     }
 }
+
+#--------------------------------------------------------------
+# Subnets
+#--------------------------------------------------------------
+
 
 
 
