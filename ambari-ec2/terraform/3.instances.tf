484bf693bf67cc3bb8a78be71c1a774c0e5456e8
CruzanCaramele <letters2hamza@gmail.com>
2017-09-26 23:29:23 +0800

    add availability zone

--- ambari-ec2/terraform/3.instances.tf
+++ ambari-ec2/terraform/3.instances.tf
@@ -3,6 +3,7 @@
 #--------------------------------------------------------------
 resource "aws_instance" "ambari-server" {
     ami               = "${var.ambari_server_ami}"
+    subnet_id         = "${element(aws_subnet.private_subnet.1.id, count.index)}"
     instance_type     = "${var.ambari_server_instance}"
-    availability_zone = ""
+    availability_zone = "${var.ambari_server_zone}"
 }
