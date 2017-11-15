0ec204f81e58a23ef2764af655d3c7e9497cbd28
CruzanCaramele <letters2hamza@gmail.com>
2017-11-16 01:11:44 +0800

    add security group for ambari server

--- ambari-ec2/terraform/3.instances.tf
+++ ambari-ec2/terraform/3.instances.tf
@@ -3,7 +3,8 @@
 #--------------------------------------------------------------
 resource "aws_instance" "ambari-server" {
     ami               = "${var.ambari_server_ami}"
-    subnet_id         = "${element(aws_subnet.private_subnet.1.id, count.index)}"
+    subnet_id         = "${element(aws_subnet.public-ambari-subnet.1.id, count.index)}"
     instance_type     = "${var.ambari_server_instance}"
+    security_groups   = "${aws_security_group.ambari-server-security}"
     availability_zone = "${var.ambari_server_zone}"
 }
