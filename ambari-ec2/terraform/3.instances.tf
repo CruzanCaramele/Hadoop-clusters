abd17f390bf0378bb232512c142f9ca144da38f1
CruzanCaramele <letters2hamza@gmail.com>
2017-11-17 02:05:32 +0800

    change instance subnet to current vpc subnet

--- ambari-ec2/terraform/3.instances.tf
+++ ambari-ec2/terraform/3.instances.tf
@@ -1,21 +1,44 @@
 #--------------------------------------------------------------
-# Ambari Server Instance 
+# Ambari Server (NameNode) Instance 
 #--------------------------------------------------------------
 resource "aws_instance" "ambari-server" {
-    ami               = "${var.ambari_server_ami}"
-    key_name          = "${var.ssh_key_name}"
-    subnet_id         = "${element(aws_subnet.public-ambari-subnet.1.id, count.index)}"
-    ebs_optimized     = "${var.ebs_variable}"
-    instance_type     = "${var.ambari_server_instance}"
-    security_groups   = "${aws_security_group.ambari-server-security}"
-    availability_zone = "${var.ambari_server_zone}"
+    ami                      = "${var.ambari_server_ami}"
+    key_name                 = "${var.ssh_key_name}"
+    #subnet_id                = "${element(aws_subnet.public-ambari-subnet.id, count.index)}"
+    subnet_id                = "${aws_subnet.public-ambari-subnet.id}"
+    ebs_optimized            = "${var.ebs_variable}"
+    instance_type            = "${var.ambari_server_instance}"
+    availability_zone        = "${var.ambari_server_zone}"
+    vpc_security_group_ids   = ["${aws_security_group.ambari-server-security.id}"]
 
     tags    {
-        Name          = "ambari-server"
+        Name                 = "ambari-server"
     }
 
     root_block_device {
-        volume_type   = "${var.volume_type}"
-        volume_size   = "${var.volume_size}"
+        volume_type          = "${var.volume_type}"
+        volume_size          = "${var.volume_size}"
     }
 }
+
+#--------------------------------------------------------------
+# Data Node Instances 
+#--------------------------------------------------------------
+# resource "aws_instance" "data-node" {
+#     ami               = "${var.ambari_server_ami}"
+#     key_name          = "${var.ssh_key_name}"
+#     subnet_id         = "${element(aws_subnet.public-ambari-subnet.1.id, count.index)}"
+#     ebs_optimized     = "${var.ebs_variable}"
+#     instance_type     = "${var.ambari_server_instance}"
+#     security_groups   = "${aws_security_group.ambari-node-security}"
+#     availability_zone = "${var.ambari_server_zone}"
+
+#     tags    {
+#         Name          = "ambari-server"
+#     }
+
+#     root_block_device {
+#         volume_type   = "${var.volume_type}"
+#         volume_size   = "${var.volume_size}"
+#     }
+# }
